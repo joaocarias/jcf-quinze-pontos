15087190e787c605d1ed3e5a6ab3bc139239d0ab
João Carias de França <joaocariasdefranca@gmail.com>
2022-02-14 23:27:50 -0300

    :unlock: Iniciando autenticacao

---
+++ QuinzePontosCore/src/Jcf.QuinzePontos.Api/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Jcf.QuinzePontos.Identidade.Models;
+using Jcf.QuinzePontos.Identidade.Repositorio;
+using Jcf.QuinzePontos.Identidade.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jcf.QuinzePontos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> AuthenticateAsync([FromBody] UserApp model)
+        {
+            var user = UserRepositorio.Get(model.UserName, model.Password);
+
+            if (user == null)
+                return NotFound(new { message = "Usuário ou Senha Inválida!" });
+
+            var token = TokenService.GenerateToken(user);
+
+            user.Password = "";
+
+            return new
+            {
+                user = user,
+                token = token,
+            };
+        }
+    }
+}
---
+++ QuinzePontosCore/src/Jcf.QuinzePontos.Identidade/Repositorio/UserRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Jcf.QuinzePontos.Identidade.Models;
+
+namespace Jcf.QuinzePontos.Identidade.Repositorio
+{
+    public static class UserRepositorio
+    {
+        public static UserApp Get(string userName, string password)
+        {
+            var users = new List<UserApp>
+            {
+                new() { Id = 1, UserName = "batman", Password = "batman", Role = "manager" },
+                new() { Id = 2, UserName = "robin", Password = "robin", Role = "employee" }
+            };
+
+            return users
+                .FirstOrDefault(x =>
+                     string.Equals(x.UserName, userName, StringComparison.CurrentCultureIgnoreCase)
+                        && x.Password.Equals(password));
+        }
+    }
+}
